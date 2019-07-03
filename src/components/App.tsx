3edff44689d5c32de61eb1f46dc33d0f67c21053
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-07-03 17:14:14 -0400

    migrate from reactype_app

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Test from './Test.tsx';
+
+type Props = {};
+
+const App = (props: Props) => {
+  const {} = props;
+
+  return (
+    <div>
+      <Test />
+      <button />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/Test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {};
+
+const Test = (props: Props) => {
+  const {} = props;
+
+  return (
+    <div>
+      <button />
+    </div>
+  );
+};
+export default Test;
